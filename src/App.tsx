cbaecedbf7102ccace04fdb421e1aeeed889841a
Saumil Patel <saumil.in.sa@gmail.com>
2025-04-26 10:47:40 +0530

    [Pre review] Add spped analytics

--- src/App.tsx
+++ src/App.tsx
@@ -171,13 +171,13 @@ function App() {
                     </li>
                   </ul>
                 </div>
-                <div>
+                {/* <div>
                   <h4 className="font-bold mb-4">Follow Us</h4>
                   <div className="flex space-x-4">
                     <a href="#" className="text-gray-300 hover:text-white">LinkedIn</a>
                     <a href="#" className="text-gray-300 hover:text-white">Twitter</a>
                   </div>
-                </div>
+                </div> */}
               </div>
               <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
                 <p>&copy; 2025 TDH Enterprises (Pty) Ltd. All rights reserved.</p>
